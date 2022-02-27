0ecd5139d377ae52789aea2600793cd17dd6476e
Greydne Scott <greydenscott@gmail.com>
2022-02-27 10:16:46 +1000

    Initial React App and Page Title
    fixed minor issue with tscoffig file

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,5 @@
-console.log('Hello World!')
+import ReactDOM from 'react-dom'
+import App from './pages/app'
+import './css/index.css'
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ src/pages/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="page-wrapper">
+      <h1>&#128221; Todo List</h1>
+    </div>
+  )
+}
+
+export default App
